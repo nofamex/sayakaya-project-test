d1289ada840418aeb2a5193fb9837fc9ed4d0d60
Nofaldi Fikrul Atmam <nsiren517@gmail.com>
2023-03-13 01:07:11 +0700

    add seeder and docs (#1)

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,10 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 /**
  * * Single and global prisma instance
+ * ! Instance will be stored as global variable only in dev environment
  */
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
